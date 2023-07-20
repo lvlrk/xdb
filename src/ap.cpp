c4ed5bf7419a776975351e181ca58548aedaaa86
lvlrk <lvlrk4u@proton.me>
2023-07-20 20:32:23 +0000

    newshit

--- src/ap.cpp
+++ src/ap.cpp
@@ -1,167 +1,31 @@
-#include "ap.h"
-#include "util.h"
-#include <fmt/core.h>
 #include <iostream>
+#include "ap.h"
 
-int ArgParser::OptionType(const std::string &arg) {
-    int ret = OPT_NO;
-    if (arg != "") {
-        if (arg[0] == '-' && arg[1] != '-') {
-            ret = OPT_SHORT;
-        } else if (arg[0] == '-' && arg[1] == '-') {
-            ret = OPT_LONG;
-        }
-    }
-
-    return ret;
-}
-
-bool ArgParser::Option::IsArgTypeSpecial() {
-    return (argType == ARG_NONE || argType == ARG_ANY);
-}
-
-ArgParser::Option::Option(const std::string &longName, char shortName,
-        int argType) {
-    this->longName = longName;
-    this->shortName = shortName;
-    this->argType = argType;
-
-    if (!IsArgTypeSpecial())
-        args.resize(argType);
-}
-
-ArgParser::ArgParser(int argc, char **argv) {
-    this->argc = argc;
-    this->argv = argv;
-
-    if (argv[0][0] == '.' && argv[0][1] == *DIRSEP)
-        program = argv[0] + 2; // remove './' from argv[0]
-    else
-        program = argv[0];
-}
-
-void ArgParser::AddOpt(Option &opt) { opts.push_back(opt); }
-
-void ArgParser::ParseArgs(int i, int j) {
-    if (opts[j].get().argType != Option::ARG_ANY) {
-        int l = 0;
-        for (int k = 0; k < opts[j].get().argType; k++) {
-            if (argv[i + k + 1]) {
-                opts[j].get().args[l++] = std::string(argv[i + k + 1]);
-            } else {
-                opts[j].get().args.push_back("");
-            }
-        }
-    } else if (opts[j].get().argType == Option::ARG_ANY) {
-        int k = 1;
-
-        while (argv[i + k]) {
-            if (OptionType(argv[i + k]) == OPT_NO) {
-                opts[j].get().args.push_back(argv[i + k]);
-            } else {
-                break;
-            }
-
-            k++;
-        }
+ap::Opt::Opt(const std::string& nLong, char nShort, int argCount):
+    nLong{nLong},
+    nShort{nShort},
+argCount{argCount} {
+    switch(argCount) {
+        case AC_NONE:
+            args.resize(0);
+            break;
+        case AC_VARI: break; // case here; not grouped with default
+        default:
+            args.resize(argCount);
+            break;
     }
 }
 
-void ArgParser::Parse() {
-    char *carg;
-    char *arga = nullptr;
-    std::string sexy;
-    char copt; // used for short options
-    bool unrecognized = false;
-    int tmp;
-    int optType;
-    int idx;
-    int tmp2;
-    std::string scarg;
-    std::string scargo;
+ap::Parser::Parser(int argc, char **argv):
+    argc{argc},
+argv{argv} {
+    args.resize(argc - 1);
 
-    for (int i = 1; i < argc; i++) {
-        // resetting the 'temporary' variables
-        unrecognized = true;
-        carg = argv[i];
-        tmp = 0;
-        arga = nullptr;
-        tmp2 = 0;
+    for(int i = 1; i < argc; i++) args[i - 1] = argv[i];
+}
 
-        optType = OptionType(argv[i]);
-        switch (optType) {
-            case OPT_LONG:
-                carg = argv[i] + 2; // remove '--' from argv[i]
-
-                for (int j = 0; j < opts.size(); j++) {
-                    idx = std::string(carg).find('=');
-
-                    if (idx != std::string::npos) {
-                        scarg = carg;
-                        scargo = scarg;
-
-                        arga = const_cast<char *>(scargo.substr(idx + 1).c_str());
-                        sexy = arga;
-
-                        carg = const_cast<char *>(scargo.erase(idx).c_str());
-                    }
-
-                    if (std::string(carg) == opts[j].get().longName) {
-                        opts[j].get().flag = true;
-                        opts[j].get().caller = fmt::format("--{:s}", carg);
-
-                        if (idx == std::string::npos) {
-                            tmp2 = opts[j].get().argType;
-
-                            if (tmp2 != 0) {
-                                opts[j].get().args.resize(tmp2);
-
-                                opts[j].get().args[tmp2 - 1] = sexy;
-                            }
-                        }
-                        unrecognized = false;
-
-                        if (opts[j].get().argType != Option::ARG_NONE &&
-                                idx != std::string::npos) {
-                            ParseArgs(i, j);
-                        }
-                    }
-                }
-
-                if (unrecognized && arga == nullptr) {
-                    std::cerr << fmt::format("{}: unrecognized option --{}\n", program,
-                            carg);
-                }
-
-                break;
-            case OPT_SHORT:
-                carg = argv[i] + 1; // remove '-' from argv[i]
-                copt = 0;
-
-                for (int j = 0; j < std::string(carg).size(); j++) {
-                    copt = carg[j];
-
-                    for (int k = 0; k < opts.size(); k++) {
-                        if (copt == opts[k].get().shortName) {
-                            opts[k].get().flag = true;
-                            opts[k].get().caller = fmt::format("-{:c}", copt);
-                            unrecognized = false;
-
-                            if (opts[k].get().argType != Option::ARG_NONE) {
-                                ParseArgs(i, k);
-                            }
-                        } else {
-                            tmp = j;
-                        }
-                    }
-                }
-
-                if (unrecognized) {
-                    std::cerr << fmt::format("{}: unrecognized option -{}\n", program,
-                            carg[tmp]);
-                }
-
-                break;
-        }
+void ap::Parser::Parse() {
+    for(const std::string& cArg: args) {
+        std::cout << cArg << '\n';
     }
 }
--- src/ap.h
+++ src/ap.h
@@ -3,36 +3,32 @@
 #include <string>
 #include <vector>
 
-class ArgParser {
-    public:
-        class Option {
-            public:
-                enum ArgType { ARG_NONE = 0, ARG_ANY = -1 };
-
-                Option(const std::string &longName, char shortName, int argType);
-                bool IsArgTypeSpecial();
-
-                std::string longName = "";
-                char shortName = 0;
-                bool flag = false;
-                int argType = ARG_NONE;
-                std::vector<std::string> args;
-                std::string caller = "";
-        };
-
-        enum OptionType { OPT_SHORT, OPT_LONG, OPT_NO };
-
-        ArgParser(int argc, char **argv);
-
-        void AddOpt(Option &opt);
-        void Parse();
-
-    private:
-        int OptionType(const std::string &arg);
-        void ParseArgs(int i, int j);
-
-        int argc;
-        char **argv;
-        std::vector<std::reference_wrapper<Option>> opts;
-        std::string program; // argv[0]
+namespace ap {
+enum ArgCount {
+    AC_NONE = 0,
+    AC_VARI = -1
 };
+
+class Opt {
+public:
+    Opt(const std::string& nLong, char nShort, int argCount);
+
+    std::string nLong;
+    char nShort;
+    int argCount;
+    std::vector<std::string> args;
+    bool set = false;
+};
+
+class Parser {
+public:
+    Parser(int argc, char **argv);
+    void Parse();
+
+    std::vector<std::reference_wrapper<const Opt>> opts;
+private:
+    int argc;
+    char **argv;
+    std::vector<std::string> args;
+};
+}
